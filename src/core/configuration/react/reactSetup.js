0f8ad051df4c2ab0fdf304d6c457918748880bf5
Alexrsnchz <alexrsnchz@gmail.com>
2025-11-12 21:57:41 +0100

    2.0.1

--- src/core/configuration/react/reactSetup.js
+++ src/core/configuration/react/reactSetup.js
@@ -2,19 +2,20 @@ import path from 'path';
 import { mkdir, writeFile, readFile } from 'fs/promises';
 import { app, html, index } from './reactData.js';
 
+/* Creates the files and folders needed by
+the framework */
 async function createConfig(targetDir) {
   const rendererPath = path.join(targetDir, 'src', 'renderer');
 
-  // Generates the framework files
   await writeFile(path.join(rendererPath, 'index.html'), html, 'utf8');
   await writeFile(path.join(rendererPath, 'index.tsx'), index, 'utf8');
   await writeFile(path.join(rendererPath, 'App.tsx'), app, 'utf8');
 
-  // Generates the src subfolder
   await mkdir(path.join(rendererPath, 'src'), { recursive: true });
 }
 
-/* Updates */
+/* Updates the electron.vite file with the
+framework configuration */
 async function updateViteConfig(targetDir) {
   const configPath = path.join(targetDir, 'electron.vite.config.ts');
   let config = await readFile(configPath, 'utf8');
